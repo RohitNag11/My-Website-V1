b6e5cb0ae68b493ef56e51ba3472d33678d921c4
RohitNag11 <rnag2018@gmail.com>
2020-08-05 15:54:02 +0100

    fixed mouse

--- main.js
+++ main.js
@@ -23,6 +23,12 @@ $(document).ready(function () {
         }, 2000);
     });
 
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+
     $('#up').on('click', function () {
         $('html, body').animate({
             scrollTop: 0
