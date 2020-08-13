960ef9029bdd06da944f2c91a567b01c497c9565
RohitNag11 <rnag2018@gmail.com>
2020-08-13 13:46:03 +0100

    added scroll progress bar

--- main.js
+++ main.js
@@ -80,6 +80,17 @@ $(document).ready(function () {
         once: true,
     });
 
+    function progressBarScroll() {
+        let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+            height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+            scrolled = (winScroll / height) * 100;
+        document.getElementById("progressBar").style.width = scrolled + "%";
+    }
+
+    window.onscroll = function () {
+        progressBarScroll();
+    };
+
     var mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
         direction: 'horizontal',
