61d8b5341b8d6669ce55cdee500dbbe7378b962d
RohitNag11 <rnag2018@gmail.com>
2021-09-10 15:09:05 +0100

    Updated build info

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 $(document).ready(function () {
+    $('.gallery-section').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        // Delay in milliseconds before popup is removed
+        //removalDelay: 300,
 
+        // Class that is added to popup wrapper and background
+        // make it unique to apply your CSS animations just to this exact popup
+        //mainClass: 'mfp-fade',
+        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+        zoom: {
+            enabled: true, // By default it's false, so don't forget to enable it
+
+            duration: 300, // duration of the effect, in milliseconds
+            easing: 'ease-in-out', // CSS transition easing function
+
+            // The "opener" function should return the element from which popup will be zoomed in
+            // and to which popup will be scaled down
+            // By defailt it looks for an image tag:
+            opener: function (openerElement) {
+                // openerElement is the element on which popup was initialized, in this case its <a> tag
+                // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        },
+        gallery: {
+            enabled: true,
+
+            preload: [1, 3],
+        }
+    });
 
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
